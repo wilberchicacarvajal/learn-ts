ff569158ca9d249799df7c727bbd37ec3e372f7d
wilberchicacarvajal <wilberpros@gmail.com>
2023-05-03 19:40:43 -0500

    Uso de enums, unknown, typeof

--- src/basics/exercise-1/app.ts
+++ src/basics/exercise-1/app.ts
@@ -22,7 +22,7 @@ let result = hasErrors ? 'tiene errores' : 'no tiene errores';
 
 // String
 const firstName = 'wilber';
-const lastName = 'galeano';
+const lastName = 'chica';
 
 let fullName = `su nombre es: ${firstName} ${lastName}, bienvenido`;
 console.log(fullName.toUpperCase());
@@ -39,13 +39,84 @@ let sports: string[] = ['futbol', 'baloncesto'];
 
 type stringOrNull = string | null;
 
-//Union types
+// Union types
 let students: Array<stringOrNull> = ['Nico', 'wilber', null, 'Rayffer', null, 'Ana']; // la t se caracteriza de un tipo genérico 
 console.log(students.filter((student) => student !== null));
 
-//Tuplas
+// Tuplas
 let exampleTuple: [string, null] = [ 'hola', null, ];
 
+// Tipos personalizados
 type statusCode = 'active' | 'inactive';
 
 let studentStatus: statusCode = 'inactive';
+
+// Any
+let working:  any = 25;
+working = 'hola';      // la palabra any es muy mala practica
+
+// Funciones
+/**
+ * funtion nombreFunción(parametro: tipoDato): tipoDatoQueRetornaLaFunción{}
+ */
+function validateValue( value: unknown ): boolean {
+
+    // unknown que es un tipo de dato que es desconocido
+    // para cualquier validación que deba hacer con el dato
+    // debo primero saber o validar que tipo de datos es
+    if (typeof(value) === 'string') {
+        console.log(value.trim().toUpperCase());
+    } else if (typeof value === 'number') {
+        console.log(value.toFixed());
+    }
+
+    if ( value === ''){
+        return false;
+    }else {
+        return true;
+    }
+}
+
+
+function getFullName(firstName: string, lastName: string): string {
+    return `${firstName} ${lastName}`;
+}
+
+// void
+function processResponse( saved: boolean): void {
+
+    if (saved) {
+        console.log('Guardado');
+    } else {
+        console.log('Ocurrió un error');
+    }
+}
+
+console.log( getFullName(firstName,lastName) );
+processResponse( true );
+
+enum Role{   //permite para crear diccionario sea de tipo numerico
+    Admin,
+    Client,
+    Users,
+    SuperAdmin 
+}
+
+let users:  Array< {name: string, role: Role } > = [
+    {
+        name: 'Pepito',
+        role: Role.Admin
+    },
+    {
+        name:'Juanito',
+        role:Role.Client
+        
+    },
+    {
+        name:'Fulanito',
+        role:Role.Client
+        
+    }
+]
+
+console.log( users.filter( (user) => user.role ===  Role.Client ) );
